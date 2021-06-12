9dc3cbfb0f4bc5d58bb29bf0e88fde431a8fb4cc
JakubOrz <80416611+JakubOrz@users.noreply.github.com>
2021-06-13 01:35:12 +0200

    Delete test_program.py
    o jeden plik za dużo

--- test_program.py
+++
@@ -1,7 +0,0 @@
-from Biblioteki import Core
-
-
-def test_huffman1():
-    coding = Core.huffman_encode(Core.prepare_chars("Data/test1.txt"))
-    encoded = Core.encode_word("SEKRET", coding)
-    assert encoded == "00110100111110"
